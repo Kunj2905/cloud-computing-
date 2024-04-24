8fc73e807827807e6e22c07016a45ef01cb91dd9
Kunj2905 <167881481+Kunj2905@users.noreply.github.com>
2024-04-24 12:19:50 +0530

    added INDEX.php

---
+++ INDEX.php
@@ -0,0 +1,222 @@
+<?php
+include 'MENU.php';
+include 'connection.php';
+
+?>
+
+
+<div id="carouselExampleFade" class=" carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://fscl01.fonpit.de/userfiles/7687254/image/NextPit-Apple-iPhone-14-Plus-vs-iPhone-14-Pro-Max.jpg" class="d-block w-100" height="700px" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.cnet.com/a/img/resize/4176b2e9cb62db9dccea926e3b5076e184e377ba/hub/2021/12/10/955a0f0d-e82f-4883-a1c3-48c9d0bec41f/splash.jpg?auto=webp&fit=crop&height=675&width=1200" class="d-block w-100" height="700px"  alt="...">
+      
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-iPhone-14-Plus-hero-220907-geo_Full-Bleed-Image.jpg.large.jpg" class="d-block w-100" height="700px"  alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button></div>
+
+
+
+  
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_01._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_02._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_03._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_04._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_05._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_06._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_07._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_08._CB609634686_.jpg" width="100%" height="100%">
+  <img src="https://m.media-amazon.com/images/G/31/img21/Wireless/katariy/Apple/2022/i14/Aplus_14/r1594_Product_Page_Flex_Module_Avail_Amazon_Desktop__en-IN_09._CB609634686_.jpg" width="100%" height="100%">
+ 
+
+  
+
+
+
+  <div class="mt-3"><h1 style="text-align: center;font-size: 35PX;color: #00bcd4;">IPHONE 14</h1></div>	
+  <section class="container mt-3">
+	<div class="card-group">
+  	<div class="card">
+    <img src="https://m.media-amazon.com/images/I/611mRs-imxL._SL1500_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 14</h5>
+      <p class="card-text">iPhone 14 256GB Midnight
+Visit the Apple Store
+ 
+₹89,900
+Inclusive of all taxes
+EMI starts at ₹4,295. No Cost EMI available </p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.amazon.in/Apple-iPhone-14-256GB-Midnight/dp/B0BDJ6N5D6/ref=sr_1_3?crid=S48UEN0XT95U&keywords=iphone+14&qid=1663415238&sprefix=%2Caps%2C433&sr=8-3">BUY NOW</a></CENTER>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://images-eu.ssl-images-amazon.com/images/I/31ja3BpXlGL._SY445_SX342_QL70_FMwebp_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 14 PLUS</h5>
+      <p class="card-text">iPhone 14 Plus 256GB Purple
+Visit the Apple Store
+ 
+₹99,900
+Inclusive of all taxes
+EMI starts at ₹4,796. No Cost EMI</p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.amazon.in/iPhone-14-Plus-256GB-Purple/dp/B0BDK63CBS/ref=sr_1_3?crid=3264H5SYO5NX&keywords=iphone+14+plus&qid=1663415413&sprefix=iphone+14+plus%2Caps%2C310&sr=8-3">BUY NOW</a></CENTER>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://m.media-amazon.com/images/I/61D84NtVgVL._SX679_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 14 PRO MAX</h5>
+      <p class="card-text">IPhone 14 Pro Max (256GB) - Silver
+Visit the Apple Store
+4.6 out of 5 stars    1,196 ratings | 280 answered questions
+-11% ₹1,23,900
+M.R.P.: ₹1,39,900
+Inclusive of all taxes
+EMI starts at ₹5,949. No Cost EMI</p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.amazon.in/Apple-iPhone-Pro-Max-256GB/dp/B09G93BPYT/ref=sr_1_7?crid=S48UEN0XT95U&keywords=iphone+14&qid=1663415238&sprefix=%2Caps%2C433&sr=8-7">BUY NOW</a></CENTER>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<div class="mt-5"><h1 style="text-align: center;font-size: 35PX;color: #00bcd4;">IPHONE 13</h1></div> 
+  <section class="container mt-3">
+  <div class="card-group">
+    <div class="card">
+    <img src="https://m.media-amazon.com/images/I/71xb2xkN5qL._SX679_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 13</h5>
+      <p class="card-text"> 15 cm (6.1-inch) Super Retina XDR display
+Cinematic mode adds shallow depth of field and shifts focus automatically in your videos
+Advanced dual-camera system with 12MP Wide and Ultra Wide cameras; Photographic Styles, Smart HDR 4, Night mode, 4K Dolby Vision HDR recording
+12MP TrueDepth front camera with Night mode, 4K Dolby Vision HDR recording
+A15 Bionic chip for lightning-fast performance
+Up to 19 hours of video playback
+Durable design with Ceramic Shield </p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.amazon.in/Apple-iPhone-13-128GB-Blue/dp/B09G9BL5CP/ref=sr_1_omk_4?crid=3BYQYL19TFORE&keywords=iphone+13&qid=1663774656&sprefix=iphone+13+%2Caps%2C250&sr=8-4">BUY NOW</a></CENTER>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://m.media-amazon.com/images/I/61eDXs9QFNL._SX679_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 13 PRO</h5>
+      <p class="card-text">15 cm (6.1-inch) Super Retina XDR display with ProMotion for a faster, more responsive feel
+Cinematic mode adds shallow depth of field and shifts focus automatically in your videos
+Pro camera system with new 12MP Telephoto, Wide and Ultra Wide cameras; LiDAR Scanner; 6x optical zoom range; macro photography; Photographic Styles, ProRes video, Smart HDR 4, Night mode, Apple ProRAW, 4K Dolby Vision HDR recording</p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.amazon.in/Apple-iPhone-13-Pro-256GB/dp/B09G9GGCZ2/ref=sr_1_3?crid=1CVG00RJUT9U0&keywords=iphone+13+pro&qid=1663775113&sprefix=iphone+13+pro%2Caps%2C523&sr=8-3">BUY NOW</a></CENTER>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://images-eu.ssl-images-amazon.com/images/I/31jgoap1cQL._SY445_SX342_QL70_FMwebp_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 13 PRO MAX</h5>
+      <p class="card-text">17 cm (6.7-inch) Super Retina XDR display with ProMotion for a faster, more responsive feel
+Cinematic mode adds shallow depth of field and shifts focus automatically in your videos
+Pro camera system with new 12MP Telephoto, Wide and Ultra Wide cameras; LiDAR Scanner; 6x optical zoom range; macro photography; Photographic Styles, ProRes video, Smart HDR 4, Night mode, Apple ProRAW, 4K Dolby Vision HDR recording
+12MP TrueDepth front camera with Night mode, 4K Dolby Vision HDR recording</p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.amazon.in/Apple-iPhone-Pro-Max-256GB/dp/B09G93BPYT/ref=sr_1_7?crid=S48UEN0XT95U&keywords=iphone+14&qid=1663415238&sprefix=%2Caps%2C433&sr=8-7">BUY NOW</a></CENTER>
+    </div>
+  </div>
+</div></section></section>
+
+<center><a class="btn top-btn" href="#navbar">  <img class="topbtnimg" src="https://uxwing.com/wp-content/themes/uxwing/download/arrow-direction/double-arrow-top-icon.png" width="10%" height="10%"> </a></center>
+
+
+
+
+
+
+
+<div class="mt-5"><h1 style="text-align: center;font-size: 35PX;color: #00bcd4;">IPHONE 12</h1></div> 
+  <section class="container mt-3">
+  <div class="card-group">
+    <div class="card">
+    <img src="https://m.media-amazon.com/images/I/417PwBC+iEL._SX342_SY445_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 12</h5>
+      <p class="card-text"> 128 GB ROM
+15.49 cm (6.1 inch) Super Retina XDR Display
+12MP + 12MP | 12MP Front Camera
+A14 Bionic Chip with Next Generation Neural Engine Processor
+Ceramic Shield
+Industry-leading IP68 Water Resistance
+All Screen OLED Display
+12MP TrueDepth Front Camera with Night Mode, 4K Dolby Vision HDR Recording </p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.flipkart.com/apple-iphone-12-black-128-gb/p/itmf1f0a58f1ecd7?pid=MOBFWBYZK3HACR72&lid=LSTMOBFWBYZK3HACR72T8MSUQ&marketplace=FLIPKART&q=iphone+12+&store=tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=b2e0fd4b-a336-418d-8d10-adf8a20006ec.MOBFWBYZK3HACR72.SEARCH&ppt=sp&ppn=sp&ssid=2h46jy47rk0000001672934885127&qH=95d6d13c062b19a0">BUY NOW</a></CENTER>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://m.media-amazon.com/images/I/61eDXs9QFNL._SL1500_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 12 PRO</h5>
+      <p class="card-text">256 GB ROM
+15.49 cm (6.1 inch) Super Retina XDR Display
+12MP + 12MP + 12MP | 12MP Front Camera
+A14 Bionic Chip with Next Generation Neural Engine Processor
+Ceramic Shield | Industry-leading IP68 Water Resistance
+All Screen OLED Display
+LiDAR Scanner for Improved AR Experiences, Night Mode Portraits</p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.flipkart.com/apple-iphone-12-pro-graphite-256-gb/p/itm4fa4da575698c?pid=MOBFWBYZBA36UB7G&lid=LSTMOBFWBYZBA36UB7GZYS7EA&marketplace=FLIPKART&q=iphone+12+pro&store=tyy%2F4io&spotlightTagId=BestvalueId_tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=bb426c63-778d-4484-a68d-68855296a9b5.MOBFWBYZBA36UB7G.SEARCH&ppt=sp&ppn=sp&ssid=dnl6e53q680000001672935011629&qH=8b8a097744d4c19d">BUY NOW</a></CENTER>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://m.media-amazon.com/images/I/61eDXs9QFNL._SL1500_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IPHONE 12 PRO MAX</h5>
+      <p class="card-text">128 GB ROM
+17.02 cm (6.7 inch) Super Retina XDR Display
+12MP + 12MP + 12MP | 12MP Front Camera
+A14 Bionic Chip with Next Generation Neural Engine Processor
+Ceramic Shield | Industry-leading IP68 Water Resistance
+All Screen OLED Display
+LiDAR Scanner for Improved AR Experiences, Night Mode Portraits</p>
+    </div>
+    <div class="card-footer">
+      <CENTER><a type="button" class="btn btn-primary btn-lg" href="https://www.flipkart.com/apple-iphone-12-pro-max-gold-256-gb/p/itm1e0354f5bbc8c?pid=MOBFWBYZZPW8JHQF&lid=LSTMOBFWBYZZPW8JHQFVJR3PO&marketplace=FLIPKART&q=iphone+12+pro+max&store=tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=87c08323-54c1-4512-b4d8-126e4dcdd09f.MOBFWBYZZPW8JHQF.SEARCH&ppt=pp&ppn=pp&ssid=cbs0s95w5c0000001672989922668&qH=5a7a12c4a730c1af">BUY NOW</a></CENTER>
+    </div>
+  </div>
+</div></section></section>
+
+<center><a class="btn top-btn" href="#navbar">  <img class="topbtnimg" src="https://uxwing.com/wp-content/themes/uxwing/download/arrow-direction/double-arrow-top-icon.png" width="10%" height="10%"> </a></center>
+
+
+
+
+
+
+
+<?php
+include 'FOOTER.php';
+?>
